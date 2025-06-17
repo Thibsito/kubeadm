96e296cdd56bc12e6e22e629b6050275ad483595
Thibault <thibault@zenops.fr>
2025-06-17 22:15:20 +0200

    creating first step

---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "harvester_kubeconfig_path" {
+    description = "Harvester cluster Kubeconfig file name with full path"
+    type        = string
+    default = "/Users/thibault/.kube/zenops-harvester.yaml"
+}
